98e69fa24e48471046691bdc37803a5856a20b01
mrbeanc <mrbeanc@qq.com>
2022-10-25 17:18:41 +0800

    版本版本！

--- updateform.h
+++ updateform.h
@@ -37,7 +37,7 @@ private:
     QWinTaskbarButton* taskBarBtn = nullptr;
     QWinTaskbarProgress* taskBarProgress = nullptr;
 
-    const QString ver = "v2.14.2";
+    const QString ver = "v2.14.3";
 
     // QWidget interface
 protected:
